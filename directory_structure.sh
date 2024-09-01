55cca1bb1f5063c7fbf831d9e309c3df7c354d56
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-09-01 16:51:05 -0400

    Delete directory_structure.sh

--- directory_structure.sh
+++
@@ -1,8 +0,0 @@
-NeoLux/
-├── neolux/           # This is your main package directory, renamed to match NeoLux
-│   ├── __init__.py
-│   ├── ...           # Other Python files
-├── setup.py          # The distutils setup script
-├── README.md         # Project description
-├── LICENSE.md        # License file
-├── requirements.txt  # Dependencies
