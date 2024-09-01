9e31e6cf84ffcd24daf845ecef7858ce6010d1b8
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-09-01 04:35:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='LuxLang',
+    name='NeoLux',
     version='1.0.0',
     author='Joey Soprano',
     author_email='violetaurac@icloud.com',
     description='LuxLang: A refined programming language with elegant syntax and dynamic features',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/JoeySoprano420/LuxLang',
+    url='https://github.com/JoeySoprano420/NeoLux',
     packages=find_packages(),
     install_requires=[
         # List of dependencies (filled in requirements.txt)
